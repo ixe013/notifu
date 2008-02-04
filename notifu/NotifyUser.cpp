24b7db86abe05c72398c07eaefa24d8be2cebdf3
Guillaume <guillaume@paralint.com>
2008-02-04 05:12:28 +0000

    Checked in rigged version by mistake... doh !

--- notifu/NotifyUser.cpp
+++ notifu/NotifyUser.cpp
@@ -4,10 +4,6 @@
 #include "NotifyUser.h"
 #include "QueryContinue.h"
 
-// {723589EC-2A89-44f4-BD3C-2992807DB4F5}
-static const GUID IID_marde = 
-{ 0x723589ec, 0x2a89, 0x44f4, { 0xbd, 0x3c, 0x29, 0x92, 0x80, 0x7d, 0xb4, 0xf5 } };
-
 //		case 0x800704C7 : errorlevel = eClosedBallon; break;
 //		case S_OK :	errorlevel = eClickedBallon; break;
 //		case S_FALSE :	errorlevel = eTimedOut; break;
@@ -18,8 +14,7 @@ HRESULT NotifyUser(const NOTIFU_PARAM& params, IQueryContinue *querycontinue)
 
 	IUserNotification *un = 0;
 
-	//result = CoCreateInstance(CLSID_UserNotification, 0, CLSCTX_ALL, IID_IUserNotification, (void**)&un); 
-	result = CoCreateInstance(CLSID_UserNotification, 0, CLSCTX_ALL, IID_marde, (void**)&un); 
+	result = CoCreateInstance(CLSID_UserNotification, 0, CLSCTX_ALL, IID_IUserNotification, (void**)&un); 
 
 	if(SUCCEEDED(result))
 	{
