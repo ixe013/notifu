0b99e73267bd0cca75e870b693930dd26a58f99a
Guillaume <guillaume@paralint.com>
2009-06-09 02:30:28 +0000

    Removed old build file

--- build.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-vcbuild /nologo /nohtmllog %* notifu.sln "Release|Win32"
-
-echo ------------------------
-echo Generating documentation
-echo ------------------------
-rd /s /q html
-doxygen doxygen\notifu.doxygen
