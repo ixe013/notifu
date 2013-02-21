f87ea43fb603a75d56dcbe9decf5096256182a1e
Guillaume <guillaume@paralint.com>
2013-02-21 05:46:04 +0000

    Changed to a Windows subsystem

--- makezip.cmd
+++ makezip.cmd
@@ -23,17 +23,15 @@ echo.
 findstr /s /n DebugBreak *.c *.cpp *.h 
 if ERRORLEVEL 1 (
 echo Building...
-    dir /s /b *.vcxproj
-    pause
     msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=Win32;Configuration=Debug
     msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=Win32;Configuration=Release
-    msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=Win64;Configuration=Debug
-    msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=Win64;Configuration=Release
+    msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=x64;Configuration=Debug
+    msbuild notifu\notifu.vcxproj /nologo /v:q /p:Platform=x64;Configuration=Release
     msbuild cnotifu\cnotifu.vcxproj /nologo /v:q /p:Platform=Win32;Configuration=Debug
     msbuild cnotifu\cnotifu.vcxproj /nologo /v:q /p:Platform=Win32;Configuration=Release
 
     echo Creating binary zip
-    zip -j -q ..\%PROJECT_NAME%.zip release\%PROJECT_NAME%.exe x64\release\%PROJECT_NAME%64.exe release\c%PROJECT_NAME%.exe release\%PROJECT_NAME%.pdb release\c%PROJECT_NAME%.pdb x64\release\%PROJECT_NAME%64.pdb Sample.bat Test.bat 
+    zip -j -q ..\%PROJECT_NAME%.zip notifu\release\%PROJECT_NAME%.exe notifu\x64\release\%PROJECT_NAME%64.exe cnotifu\release\c%PROJECT_NAME%.exe notifu\release\%PROJECT_NAME%.pdb cnotifu\release\c%PROJECT_NAME%.pdb notifu\x64\release\%PROJECT_NAME%64.pdb Sample.bat Test.bat 
 ) else (
     echo.
     echo Some files or binaries where not build. Fix it or die.
